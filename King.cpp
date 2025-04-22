a42cde2af723e3cb0d395fce7621fdea8427637b
Johnathan Njau <jibekwe1>
2025-04-21 22:05:19 -0400

    Initalized the legal_move_shape function for king

--- King.cpp
+++ King.cpp
@@ -1,12 +1,27 @@
 #include "King.h"
+#include "Board.h"
 #include <cmath>
 
 namespace Chess
 {
   bool King::legal_move_shape(const Position& start, const Position& end) const {
-    /////////////////////////
-    // [REPLACE THIS STUB] //
-    /////////////////////////
+    
+    int dx = abs(end.first - start.first);
+    int dy = abs(end.second - start.second);
+    // keeps dx and dy in the range of 0 - 1 as thats how far the king can
+    // move x and y. And if both dy and dx are 0, then the king didnt move
+    if (dx <= 1 && dy <=1 && (dy != 0 || dx != 0)) {
+      if ((board->isOccupied(Position(end.first,end.second)))) {
+        const Piece* target = board->operator()(Position(end.first, end.second));
+        // checks if target is an enemy
+        if ( target && (this->is_white() != target->is_white()) ) {
+          return true;
+        }
+      }
+      return true;
+    }
+
     return false;
   }
+
 }
