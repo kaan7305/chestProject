2b79d765928e7e2787177651d5bee0e4cb18bd38
Kaan <kaaneroltu73@gmail.com>
2025-04-23 01:39:30 -0400

    Knight finished

--- Knight.cpp
+++ Knight.cpp
@@ -1,12 +1,19 @@
 #include "Knight.h"
 #include <cmath>
+using std::abs;
 
 namespace Chess
 {
   bool Knight::legal_move_shape(const Position& start, const Position& end) const {
-    /////////////////////////
-    // [REPLACE THIS STUB] //
-    /////////////////////////
-    return false;
+
+    int dx = abs(end.first - start.first);
+
+    int dy = abs(end.second - start.second);
+
+    // We know that knight moves in L shape
+    
+    return (dx == 1 && dy == 2) || (dx == 2 && dy == 1);
+    
   }
 }
+
