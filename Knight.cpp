f387e5a2871e0ed476ff986dba0eede38128ca89
Johnathan Njau <jibekwe1>
2025-04-28 21:36:09 -0400

    Fixed formating in Knight

--- Knight.cpp
+++ Knight.cpp
@@ -3,41 +3,35 @@
 #include <cmath>
 using std::abs;
 
-namespace Chess
-{
-  bool Knight::legal_move_shape(const Position& start, const Position& end) const {
+namespace Chess {
+bool Knight::legal_move_shape(const Position &start,
+                              const Position &end) const {
 
-    // we are checking if tge move is in the board and valid
-    int dx = abs(end.first - start.first);
+  // we are checking if tge move is in the board and valid
+  int dx = abs(end.first - start.first);
 
-    int dy = abs(end.second - start.second);
+  int dy = abs(end.second - start.second);
 
-    // We know that knight moves in L shape 
-    // so we can check if the move is valid
-    // Also we have to chec if the move is legit by checking if it is white or black
-    
-    if ((dx == 1 && dy == 2) || (dx == 2 && dy == 1))
-    {
-      // this will help us to see if the move is occupied or not
-      if ((board->isOccupied(Position(end.first,end.second)))) 
-      {
-        const Piece* target = board->operator()(Position(end.first, end.second));
-        // we are checking if target is an enemy one
-        if (target && (this->is_white() != target->is_white())) 
-        {
-          return true;
-        }
-        return false;
+  // We know that knight moves in L shape
+  // so we can check if the move is valid
+  // Also we have to chec if the move is legit by checking if it is white or
+  // black
+
+  if ((dx == 1 && dy == 2) || (dx == 2 && dy == 1)) {
+    // this will help us to see if the move is occupied or not
+    if ((board->isOccupied(Position(end.first, end.second)))) {
+      const Piece *target = board->operator()(Position(end.first, end.second));
+      // we are checking if target is an enemy one
+      if (target && (this->is_white() != target->is_white())) {
+        return true;
       }
-      return true;
+      return false;
     }
-
-    return false;
+    return true;
   }
 
-  int Knight::point_value() const {
-    return 3;
+  return false;
 }
-}
-    
 
+int Knight::point_value() const { return 3; }
+}
