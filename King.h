c758ab94d3703b165c411200c4607977eddf4f80
Johnathan Njau <jibekwe1>
2025-04-28 22:09:02 -0400

    Added comments

--- King.h
+++ King.h
@@ -8,18 +8,24 @@ namespace Chess
 	class King : public Piece {
 
 	public:
-    bool legal_move_shape(const Position& start, const Position& end) const override;
+		// returns true if the move is legal for this piece
+    	bool legal_move_shape(const Position& start, const Position& end) const override;
 
+		// ASCII representation of the piece
 		char to_ascii() const override { return is_white() ? 'K' : 'k';	}
 
+		// Unicode representation of the piece
 		std::string to_unicode() const override { return is_white() ? "\u2654" : "\u265A"; }
 
+		// returns piece's point value
 		int point_value() const override;
     
 	private:
 
+		// Constructor
 		King(bool is_white) : Piece(is_white) {}
 
+		// creates a new piece
 		friend Piece* create_piece(const char& piece_designator);
 	};
 
