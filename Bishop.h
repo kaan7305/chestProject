d0994fe636f994c7885b77c6762fde59c7ad7909
Johnathan Njau <jibekwe1>
2025-04-28 22:07:10 -0400

    Added more comments

--- Bishop.h
+++ Bishop.h
@@ -8,24 +8,24 @@ namespace Chess
 	class Bishop : public Piece {
 
 	public:
+	// returns true if the move is legal for this piece
     bool legal_move_shape(const Position& start, const Position& end) const override;
 
-		/////////////////////////////////////
-		// DO NOT MODIFY THIS FUNCTION!!!! //
-		/////////////////////////////////////
+	    // ASCII representation of the piece
 		char to_ascii() const override { return is_white() ? 'B' : 'b';	}
     
-    /////////////////////////////////////
-		// DO NOT MODIFY THIS FUNCTION!!!! //
-		/////////////////////////////////////
+		// Unicode representation of the piece
 		std::string to_unicode() const override { return is_white() ? "\u2657" : "\u265D"; }
-	int point_value() const override;
+		
+		// returns piece's point value
+		int point_value() const override;
 	private:
-		/////////////////////////////////////
-		// DO NOT MODIFY THIS FUNCTION!!!! //
-		/////////////////////////////////////
+		
+		// Constructor
 		Bishop(bool is_white) : Piece(is_white) {}
 
+		// creates a new piece
+		// based on the piece designator
 		friend Piece* create_piece(const char& piece_designator);
 	};
 }
